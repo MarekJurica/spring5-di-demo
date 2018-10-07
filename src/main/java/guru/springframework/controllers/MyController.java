7b9d0bfa070a158a1b424b05ac34df1862fee391
MarekJurica <marek.jurica@centrum.cz>
2018-10-07 18:31:00 +0200

    adding Primary example

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -2,12 +2,18 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.services.GreetingService;
+
 @Controller
 public class MyController {
     
-    public String hello() {
-        System.out.println("Hello!!!");
-        
-        return "foo";
+    private GreetingService greetingService;
+    
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String hello() {       
+        return greetingService.sayGreeting();
     }
 }
