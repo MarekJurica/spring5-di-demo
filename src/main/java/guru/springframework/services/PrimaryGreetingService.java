3d48f088ff405ef92604337b19616ea50eff66a8
MarekJurica <marek.jurica@centrum.cz>
2018-10-08 17:14:01 +0200

    adding reviewed code

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -13,7 +13,11 @@ import org.springframework.stereotype.Service;
 @Profile({"en", "default"})
 public class PrimaryGreetingService implements GreetingService {
 
-    private GreetingRepository greetingRepository;
+    private GreetingRepository greetingRepository;    
+    
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
