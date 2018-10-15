52552efec54d96d890fa9944edf8240f655576ae
MarekJurica <marek.jurica@centrum.cz>
2018-10-15 13:27:46 +0200

    refactoring to use property files

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -2,12 +2,13 @@ package guru.springframework;
 
 import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.SetterInjectedController;
+import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 import guru.springframework.controllers.MyController;
 import guru.springframework.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DiDemoApplication {
@@ -16,11 +17,20 @@ public class DiDemoApplication {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
 		MyController controller = (MyController) ctx.getBean("myController");
-
+		
 		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+                System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+                System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+                System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+                
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		
+		System.out.println(fakeDataSource.getUser());
+		
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+                
+                System.out.println(fakeJmsBroker.getUsername());
+
 	}
 }
  
