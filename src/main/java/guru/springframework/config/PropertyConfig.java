b109e40e6202f60eda5204bab5f8cc1e09da0adf
MarekJurica <marek.jurica@centrum.cz>
2018-10-15 13:46:09 +0200

    refactoring to use the application.properties

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -3,19 +3,11 @@ package guru.springframework.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
 
 @Configuration
-@PropertySources({
-    @PropertySource("classpath:jms.properties"),
-    @PropertySource("classpath:datasource.properties")
-})
-
 public class PropertyConfig {
     
     @Value("${guru.username}")
@@ -55,11 +47,4 @@ public class PropertyConfig {
         
         return fakeJmsBroker;
     }
-    
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
-
 }
